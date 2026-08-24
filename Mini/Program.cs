a2ea20cf2c5c9ff3c0d7a75a890a045f14412c60
minty-developer <gimminseung036@gmail.com>
2026-08-24 17:29:04 +0900

    a1.0.4

--- Mini/Program.cs
+++ Mini/Program.cs
@@ -5,17 +5,20 @@ using DotNetEnv;
 
 Env.Load();
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateEmptyBuilder(new WebApplicationOptions
+{
+    Args = args
+});
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Configuration.Sources.Clear();
+// 2. 파일 감시(reloadOnChange: false)를 완전히 끈 상태로 설정 로드
 builder.Configuration
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: false)
     .AddEnvironmentVariables();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 // 2. Discord.Net 클라이언트 설정 및 DI 등록
 builder.Services.AddSingleton(options => new DiscordSocketConfig
